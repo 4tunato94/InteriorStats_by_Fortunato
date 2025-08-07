cff73783f9c001a81a3d8a0fc32d76e43ffdc5cb
4tunato94 <danielisfortunato@gmail.com>
2025-08-06 23:16:01 -0300

    Simplificar configuração do Expo

--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Configure for web platform
-config.resolver.platforms = ['web', 'ios', 'android', 'native'];
-
-// Ensure proper asset resolution
-config.resolver.assetExts.push('svg');
-
-// Configure server for better web support
-config.server = {
-  port: 8081,
-  host: 'lan',
-};
-
 module.exports = config;
