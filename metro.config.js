40df5169fb586b9a9a5ea7bec7b5df3edbc90355
4tunato94 <danielisfortunato@gmail.com>
2025-08-06 23:08:46 -0300

    Updated app.json

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,20 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable web support
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
+// Configure for better development server connectivity
+config.server = {
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      // Enable CORS for development
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = config;
